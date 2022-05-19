0442964d48e7243c4305fc8096b6976b577874d5
timwenger <61519900+timwenger@users.noreply.github.com>
2022-05-18 19:28:37 -0700

    removed tour of heroes components and services

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -6,12 +6,8 @@ import { ReactiveFormsModule, } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { TransactionsComponent } from './transactions/transactions.component';
 import { TransactionsFilterComponent } from './transactions-filter/transactions-filter.component';
 
@@ -23,11 +19,7 @@ import {MultiSelectModule} from 'primeng/multiselect';
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent,
-    HeroSearchComponent,
     TransactionsComponent,
     TransactionsFilterComponent,
   ],
